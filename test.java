aa4b4581a26f92e16e941cde8539c4ac0798b746
safwanaec <ashraisafwan@gmail.com>
2024-11-27 09:20:35 +0600

    new commit

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test{
     public static void main(String[] args) {
         System.out.println("dsfg");
         System.out.println("hello world");
+        System.out.println("new commit");
     }
 }
